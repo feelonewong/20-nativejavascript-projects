b56d116a324481323ab37104d24bcfdd443a33fc
feelonewong <feelonewong@gmail.com>
2021-01-20 21:29:49 +0800

    Wrapping public functions

--- src/project1_formValidator/App.js
+++ src/project1_formValidator/App.js
@@ -2,7 +2,7 @@ let form  = document.getElementById("form");
 let username  = document.getElementById("username");
 let email  = document.getElementById("email");
 let password  = document.getElementById("password");
-let passwordConfirm  = document.getElementById("passwordConfirm");
+let passwordConfirm  = document.getElementById("confirmPassword");
 
 function showError(node, message){
     let formControl = node.parentElement;
@@ -16,12 +16,34 @@ function showSuccess(node){
     formControl.className = "form-control success";
 
 }
-
+function isValidatorFormatter(inputArray){
+    inputArray.forEach(function (input) {
+        if(!input.value){
+            showError(input,`${input.name}为必填项`);
+            return false;
+        }else{
+            showSuccess(input);
+        }
+    })
+}
+function isCheckLength(input, min, max){
+    if(input.value.length<min){
+        showError(input,`${input.name}长度不能小于${min};`);
+    }else if(input.value.length>max){
+        showError(input,`${input.name}长度不能大于${max}`);
+    }else{
+        showSuccess(input);
+    }
+}
+function isCheckEmail(input){
+    const reg = /^[A-Za-z0-9]+([_\.][A-Za-z0-9]+)*@([A-Za-z0-9\-]+\.)+[A-Za-z]{2,6}$/;
+    let isEmailType =  reg.test(input.value);
+    !isEmailType? showError(input,`${input.name}格式错误`) : showSuccess(input);
+}
 form.addEventListener("submit", function (e){
     e.preventDefault();
-    if(!username.value){
-        showError(username,"请输入用户名");
-    }else{
-        showSuccess(username);
-    }
-})
+    isValidatorFormatter([username,email,password,passwordConfirm]);
+    isCheckLength(username,3,6);
+    isCheckLength(password, 6,17);
+    isCheckEmail(email);
+});
